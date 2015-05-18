fc19f082f79b83e36420fc9d1626861b89b4d66a
sagabajaj <sagarbajaj@abc.com>
2015-05-18 15:48:35 +0530

    nknl

---
+++ mainProject/src/main/java/com/main/pro/iTMP.java
@@ -0,0 +1,17 @@
+package com.main.pro;
+
+import com.sagar.prac.Train;
+
+/**
+ * Hello world!
+ *
+ */
+public class iTMP 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("gvgv");
+        Train.main(args);
+        
+    }
+}
---
+++ mainProject/src/test/java/com/main/pro/FRACTEST.java
@@ -0,0 +1,16 @@
+package com.main.pro;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class FRACTEST {
+@Before
+public void setup(){}
+@Test
+public void testmain(){}
+}
